38855ac16536da9a19b4f50d293725769f161122
GALAXY127 <64964447+GALAXY127@users.noreply.github.com>
2021-06-22 18:17:50 +0200

    organisera filer och folder

---
+++ src/Component/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./Header";
+import CardList from "./ContactList";
+import AddContact from "./AddContact";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
